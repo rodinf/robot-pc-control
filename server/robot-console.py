15bdabecf70e59a5162b68c542f8a8296fd47554
Fedor <apache32@yandex.ru>
2018-12-01 23:54:39 +0300

    add console

--- server/robot-console.py
+++ server/robot-console.py
@@ -1,14 +1,8 @@
-from tkinter import *
-from tkinter import ttk
-import serial
-import string
-import time
-import os
-import threading
-import random
-import hashlib
+import serial, string, time, os, random, hashlib, logging
 from flask import *
 from flask_bootstrap import Bootstrap
+
+ROBOT_VERSION = 'v1.1 Alpha'
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -21,7 +15,9 @@ def token_generate(length):
 real_token = token_generate(128)
 webuser_username = 'robot'
 webuser_password = token_generate(8)
-real_token_text = 'password:   '+webuser_password
+real_token_text = '\n-- PASSWORD:   '+webuser_password+'   --\n'
+
+print('\n-- ROBOT VASILY SERVER '+ROBOT_VERSION+' -- \n '+real_token_text)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -48,26 +44,12 @@ def com_connect():
         ser = serial.Serial(com.get(), var.get())
         time.sleep(3)
         print('Successfully connected to <{0}> !'.format(com.get()))
-        bottom_label.config(text='Successfully connected to {0}!'.format(com.get()))
     except Exception:
         print('Error: Could not connect to <{0}> !'.format(com.get()))
-        bottom_label.config(text='Could not connect to <{0}> !'.format(com.get()))
 
-root = Tk()
-root.title("Robot server")
-root.geometry('360x100')
-root.resizable(False, False)
-imgicon = PhotoImage(file=os.path.join(os.path.realpath('icon.gif')))
-root.tk.call('wm', 'iconphoto', root._w, imgicon)
-'''
-except Exception:
-    print('Error: No device connected!')
-    bottom_label.config(text='No device connected!')
-'''
 def no_conn_exeption():
     print('Error: No device connected!')
-    bottom_label.config(text='No device connected!')
-
+    
 def stop(event):
     try:
         ser.write(b's')
@@ -101,52 +83,12 @@ def move_backward(event):
         print("Moving Backward!")
     except:
         no_conn_exeption()
-
-def about():
-    top = Toplevel()
-    top.title("About")
-    top.geometry('300x150')
-    msg = Message(top, text="«Robot Vasily server» v1.1 Alpha (Non-stable!) by @badfedor, 2018. \n\n Use keys W, A, D, X \n to move forward, left, right, backward \n and key S to stop. \n\n Don't forget to check you're using english keymap! ", width=300)
-    msg.pack()
-
+		
 def combine_funcs(*funcs):
     def combined_func(*args, **kwargs):
         for f in funcs:
             f(*args, **kwargs)
     return combined_func
-
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="About", command=about)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
-imgicon_about = PhotoImage(file=os.path.join(os.path.realpath('icon.gif')))
-menubar.tk.call('wm', 'iconphoto', menubar._w, imgicon_about)
-root.config(menu=menubar)
-
-frame_com = Frame(root, bd=0)
-com = ttk.Entry(frame_com, width=20)
-com.pack(side=LEFT)
-var = StringVar(root)
-option = ttk.OptionMenu(frame_com, var, 300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 74880, 115200, 230400, 250000, 500000, 1000000, 2000000)
-var.set(9600)
-option.config(width=6)
-option.pack(side=LEFT)
-com_button = ttk.Button(frame_com, text="Connect", command=com_connect, width=10)
-com_button.pack(side=TOP)
-
-bottom_label = Label(root, text="", bd=5)
-bottom_token = Label(root, text=real_token_text, bd=5)
-frame_com.pack(side=TOP)
-bottom_token.pack()
-bottom_label.pack(side=LEFT)
-
-root.bind("<w>", move_forward)
-root.bind("<a>", move_left)
-root.bind("<d>", move_right)
-root.bind("<x>", move_backward)
-root.bind("<s>", stop)
 
 @app.route('/control', methods = ['GET'])
 def moving():
@@ -203,14 +145,5 @@ def moving():
     resp = Response(js, status=200, mimetype='application/json')
     return resp
 
-def flask_start():
-    app.run('0.0.0.0', port='8080')
-
-def tkinter_start():
-    root.mainloop()
-
 if __name__ == "__main__":
-    flt = threading.Thread(target=flask_start)
-    flt.daemon = True
-    flt.start() 
-    tkinter_start() 
+    app.run('0.0.0.0', port='8080')
